34fdd0b3f42aa3c48f37c30b78d81e6353dfc51a
zach taylor <taylor67469@yahoo.com>
2021-12-29 08:01:38 -0600

    fixing typo errors

--- src/components/projects/expense/expense.js
+++ src/components/projects/expense/expense.js
@@ -10,7 +10,7 @@ const Expense=()=>{
         <div className='par'>
         <p>A brilliant web application for tracking your expenses.</p>
         <br/>
-        <p>This application utilizies Chart.js to show your tracked expenses, as well as having a working login feature.</p>
+        <p>This application utilizes Chart.js to show your tracked expenses, as well as having a working login feature.</p>
         <br/>
         <br/>
         <p>Link: <a href="https://github.com/levickane/expensetracker7000">Github</a></p>
